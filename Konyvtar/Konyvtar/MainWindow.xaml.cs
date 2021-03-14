95885d22e85199d9ef63f18d03b7a82a96734e33
BercziZoltan <71635710+BercziZoltan@users.noreply.github.com>
2021-03-14 16:01:27 +0100

    1.0.1
    A program kód megírásának elkezdése.

--- Konyvtar/Konyvtar/MainWindow.xaml.cs
+++ Konyvtar/Konyvtar/MainWindow.xaml.cs
@@ -21,6 +21,50 @@ namespace Konyvtar
     /// </summary>
     public partial class MainWindow : Window
     {
+        class Adatok
+        {
+            public int ID { get; set; }
+            public string szerzo { get; set; }
+            public string cim { get; set; }
+            public string ev { get; set; }
+            public string kiado { get; set; }
+            public bool ig { get; set; }
+            public Adatok(string sor)
+            {
+                string[] resz = sor.Split(';');
+                ID = Convert.ToInt32(resz[0]);
+                szerzo = resz[1];
+                cim = resz[2];
+                ev = resz[3];
+                kiado = resz[4];
+                ig = Convert.ToBoolean(resz[5]);
+
+            }
+        }
+        class Adatok2
+        {
+            public int olvasID { get; set; }
+            public string nev { get; set; }
+            public DateTime szul { get; set; }
+            public int szam { get; set; }
+            public string telepules { get; set; }
+            public string utca { get; set; }
+            public Adatok2(string sor)
+            {
+                string[] resz = sor.Split(';');
+                olvasID = Convert.ToInt32(resz[0]);
+                nev = resz[1];
+                szul = Convert.ToDateTime(resz[2]);
+                szam = Convert.ToInt32(resz[3]);
+                telepules = resz[4];
+                utca = resz[5];
+            }
+            foreach (var item in File.ReadAllLines("tagok.txt"))
+            {
+                l2.Add(new Adatok2(item));
+                Tag.ItemsSource = l2;
+                Tag.AutoGenerateColumns = false;
+            }
         public MainWindow()
         {
             InitializeComponent();
